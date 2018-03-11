60f26a699261650dbc75074c4a15919e6b077bd8
unknown <l3szcz@gmail.com>
2018-03-11 11:18:46 +0100

    Creating a Responsive Sidedrawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,7 +2,6 @@ import React from "react";
 import classes from "./Toolbar.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
-import NavigationItem from "../NavigationItems/NavigationItem/NavigationItem";
 
 const Toolbar = props => (
   <header className={classes.Toolbar}>
