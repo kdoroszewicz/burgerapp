978a679e7454095a551cd2ae7544b4279671766e
unknown <l3szcz@gmail.com>
2018-03-17 11:55:41 +0100

    Navigating Back & To Next Page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -9,12 +9,24 @@ class Checkout extends Component {
       cheese: 1,
       bacon: 1
     }
-  }
+  };
+
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  };
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace("/checkout/contact-data");
+  };
 
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients} />
+        <CheckoutSummary
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}
+          ingredients={this.state.ingredients}
+        />
       </div>
     );
   }
