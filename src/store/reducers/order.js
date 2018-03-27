ce41e85a95aaf692f6b3f6158fb992e3c84ffaa2
unknown <l3szcz@gmail.com>
2018-03-27 13:14:17 +0200

    Fetching Orders (via Redux)

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -28,6 +28,22 @@ export default (state = initialState, action) => {
       return { ...state, loading: false };
     case actionTypes.PURCHASE_BURGER_START:
       return { ...state, loading: true };
+    case actionTypes.FETCH_ORDERS_START:
+      return {
+        ...state,
+        loading: true
+      };
+    case actionTypes.FETCH_ORDERS_SUCCESS:
+      return {
+        ...state,
+        orders: action.orders,
+        loading: false
+      };
+    case actionTypes.FETCH_ORDERS_FAIL:
+      return {
+        ...state,
+        loading: false
+      };
     default:
       return state;
   }
